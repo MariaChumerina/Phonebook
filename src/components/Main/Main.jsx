0c8545f1a8bf58eb403c8e66832422978fcd1656
MariaChumerina <masha.chumerina@gmail.com>
2020-07-29 16:13:50 +0500

    add search input and button for search and add contacts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import List from '../List/List.jsx';
+import Search from '../Search/Search.jsx';
 
 export default function Main() {
   return (
-      <div>
+      <div className="margin-top-50">
         <h1>
           Телефонный справочник
         </h1>
+        <Search />
+        <button>
+          Добавить контакт
+        </button>
         <h2>
           Список контактов:
         </h2>
