d473e99b00fb95f817fcc44be85d69db3c9866d6
MariaChumerina <masha.chumerina@gmail.com>
2020-07-29 17:18:33 +0500

    styled block contacts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,29 @@
 import * as React from 'react';
 import List from '../List/List.jsx';
 import Search from '../Search/Search.jsx';
+import './Main.css';
 
-export default function Main() {
-  return (
-      <div className="margin-top-50">
-        <h1>
-          Телефонный справочник
-        </h1>
-        <Search />
-        <button>
-          Добавить контакт
-        </button>
-        <h2>
-          Список контактов:
-        </h2>
-        <List />
-      </div>
-  );
+export default class Main extends React.Component {
+
+  render() {
+    return (
+        <div className="margin-top-50">
+          <h1>
+            Телефонный справочник
+          </h1>
+          <Search />
+          <div className="contacts-container margin-top-50">
+            <div className="contacts-title-container">
+              <h2>
+                Контакты
+              </h2>
+              <button className="btn-link btn-link-position">
+                &#43;
+              </button>
+            </div>
+            <List />
+          </div>
+        </div>
+    );
+  }
 }
