e7c8ba542120b6c133dd518ec4996fc681751d62
MariaChumerina <masha.chumerina@gmail.com>
2020-07-29 19:02:37 +0500

    rename function

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 import './Modal.css';
 
 export default function Modal({ handleClose }) {
-  const handleClickRemove = React.useCallback(() => handleClose(), [handleClose]);
+  const handleClickClose = React.useCallback(() => handleClose(), [handleClose]);
 
   return (
       <div className="modal">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h3 className="modal-title">Добавление контакта</h3>
-              <button className="btn-link close" onClick={handleClickRemove}>&#10005;</button>
+              <h3 className="modal-title">
+                Добавление контакта
+              </h3>
+              <button className="btn-link close" onClick={handleClickClose}>
+                &#10005;
+              </button>
             </div>
             <div className="modal-body">
               <div>
