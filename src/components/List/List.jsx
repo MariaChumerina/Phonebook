9b9e0b2afd485bb41b6a2658975dbca98c42caee
MariaChumerina <masha.chumerina@gmail.com>
2020-07-30 18:32:59 +0500

    refactor class component to function

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,26 +3,23 @@ import { connect } from 'react-redux';
 import './List.css';
 import ListItem from '../ListItem/ListItem.jsx';
 
-class List extends React.Component {
-  renderListItems = () => {
-    const { contacts } = this.props;
+const List = ({ contacts }) => {
+  const renderListItems = () => {
     return contacts.map((contact) => {
       const { name, number } = contact;
       return <ListItem name={name} number={number} key={number}/>
     })
   }
 
-  render() {
-    const { contacts } = this.props;
-    return (
-      <div>
-        <ul>
-          {contacts.length ? this.renderListItems() : 'Пусто'}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <ul>
+        {contacts.length ? renderListItems() : 'Пусто'}
+      </ul>
+    </div>
+  );
 }
+
 const mapStateToProps = state => {
   const contacts = state.filteredContacts.contact.length
       ? state.contacts.contacts.filter((contact) => {
