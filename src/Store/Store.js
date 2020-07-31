3669a90bf4736e89a70fca67e120ce77577b15ad
MariaChumerina <masha.chumerina@gmail.com>
2020-07-31 18:52:07 +0500

    utils moved

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,7 +2,7 @@ import { combineReducers, createStore } from 'redux';
 import { modalReducer } from './reducers/modalReducer.js';
 import { contactsReducer } from './reducers/contactsReducer.js';
 import { contactsSearchReducer } from './reducers/contactsSearchReducer.js';
-import { loadState, saveState } from '../components/utils/sessionSaver.js';
+import { loadState, saveState } from '../utils/sessionSaver.js';
 
 const oldState = loadState();
 
